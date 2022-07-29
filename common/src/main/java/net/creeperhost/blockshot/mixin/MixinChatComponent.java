2b1a25c039a30c874ad92a7b816b7e0dc6f1b5f3
Gigabit101 <thegigabit101@gmail.com>
2022-07-29 12:41:15 +0100

    Fix chat messages not being removed

--- common/src/main/java/net/creeperhost/blockshot/mixin/MixinChatComponent.java
+++ common/src/main/java/net/creeperhost/blockshot/mixin/MixinChatComponent.java
@@ -1,18 +1,25 @@
 package net.creeperhost.blockshot.mixin;
 
+import net.minecraft.client.GuiMessage;
 import net.minecraft.client.GuiMessageTag;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MessageSignature;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.List;
 
 @Mixin(ChatComponent.class)
 public interface MixinChatComponent {
     @Invoker("addMessage")
     void invokeaddMessage(Component component, MessageSignature messageSignature, @Nullable GuiMessageTag guiMessageTag);
 
-    @Invoker("deleteMessage")
-    void invokedeleteMessage(MessageSignature messageSignature);
+    @Invoker("refreshTrimmedMessage")
+    void invokerefreshTrimmedMessage();
+
+    @Accessor("allMessages")
+    List<GuiMessage> getAllMessages();
 }
