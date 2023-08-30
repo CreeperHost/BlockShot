26fdc4ebd9fab4f3e92f24cba7e78b549675f8c8
brandon3055 <brandon4261@gmail.com>
2023-08-31 09:39:21 +1000

    Implemented new MT session support

--- common/src/main/java/net/creeperhost/blockshot/Auth.java
+++ common/src/main/java/net/creeperhost/blockshot/Auth.java
@@ -1,24 +1,10 @@
 package net.creeperhost.blockshot;
 
-import com.google.common.hash.Hashing;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.mojang.authlib.GameProfile;
-import dev.architectury.platform.Platform;
-import net.creeperhost.minetogether.session.MineTogetherSession;
-import net.creeperhost.minetogether.session.SessionValidationException;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.User;
-import net.minecraft.core.UUIDUtil;
-import org.apache.commons.codec.digest.DigestUtils;
+import net.creeperhost.minetogether.session.JWebToken;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Locale;
-import java.util.Random;
-import java.util.UUID;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Created by brandon3055 on 23/03/2023
@@ -27,58 +13,10 @@ public class Auth {
 
     private static final String CH_API = "https://api.creeper.host/";
     private static final Logger LOGGER = LogManager.getLogger();
-
-    private static User user;
-    private static GameProfile profile;
-    private static String uuidHash;
-
-    private static boolean chAuthenticated = false;
     private static boolean hasPremium = false;
 
-    public static MineTogetherSession mtSession;
-
-    public static void init() {
-        Minecraft mc = Minecraft.getInstance();
-        user = mc.getUser();
-        profile = user.getGameProfile();
-
-        UUID playerUUID = UUIDUtil.getOrCreatePlayerUUID(profile);
-        String playerName = profile.getName();
-        uuidHash = Hashing.sha256().hashString(playerUUID.toString(), UTF_8).toString().toUpperCase(Locale.ROOT);
-
-        mtSession = new MineTogetherSession(Platform.getGameFolder(), playerUUID, playerName, Auth::doMojangAuth);
-
-        try {
-            mtSession.validate();
-            chAuthenticated = true;
-            checkMTPremiumStatus();
-        } catch (SessionValidationException e) {
-            LOGGER.error("Failed to validate session", e);
-        }
-    }
-
-    public static String doMojangAuth() {
-        String mojangServerId = DigestUtils.sha1Hex(String.valueOf(new Random().nextInt()));
-        try {
-            Minecraft.getInstance().getMinecraftSessionService().joinServer(profile, user.getAccessToken(), mojangServerId);
-            return mojangServerId;
-        } catch (Throwable e) {
-            LOGGER.error("Failed to validate with Mojang", e);
-        }
-        return "";
-    }
-
-    public static boolean hasCreeperHostAuth() {
-        return chAuthenticated;
-    }
-
-    private static void checkMTPremiumStatus() {
-        hasPremium = false;
-        if (uuidHash == null) {
-            return;
-        }
-
-        String profile = WebUtils.post(CH_API + "minetogether/profile", "{\"target\":\""+uuidHash+"\"}", WebUtils.MediaType.JSON, null);
+    public static void init(JWebToken auth) {
+        String profile = WebUtils.post(CH_API + "minetogether/profile", "{\"target\":\"" + auth.getUuidHash() + "\"}", WebUtils.MediaType.JSON, null);
         if ("error".equals(profile)) {
             return;
         }
@@ -89,7 +27,7 @@ public class Auth {
                 return;
             }
 
-            JsonObject data = response.getAsJsonObject("profileData").getAsJsonObject(uuidHash);
+            JsonObject data = response.getAsJsonObject("profileData").getAsJsonObject(auth.getUuidHash());
             hasPremium = data.get("premium").getAsBoolean();
         }catch (Throwable e) {
             LOGGER.error("An error occurred while retrieving MineTogether profile", e);
--- common/src/main/java/net/creeperhost/blockshot/BlockShot.java
+++ common/src/main/java/net/creeperhost/blockshot/BlockShot.java
@@ -1,35 +1,57 @@
 package net.creeperhost.blockshot;
 
+import dev.architectury.event.events.client.ClientLifecycleEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import net.creeperhost.blockshot.gui.GuiEvents;
+import net.creeperhost.blockshot.lib.MTSessionProvider;
 import net.creeperhost.blockshot.mixin.MixinMinecraft;
+import net.creeperhost.minetogether.session.JWebToken;
+import net.creeperhost.minetogether.session.MineTogetherSession;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.MessageSignature;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 
 import java.nio.file.Path;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class BlockShot {
     public static final String MOD_ID = "blockshot";
     public static final Logger LOGGER = LogManager.getLogger();
     public static Path configLocation = Platform.getGameFolder().resolve(MOD_ID + ".json");
+    private static boolean active = false;
+    private static CompletableFuture<@Nullable JWebToken> tokenFuture;
     public static final MessageSignature CHAT_UPLOAD_ID = new MessageSignature(new byte[]{36, 03, 60});
     public static final MessageSignature CHAT_ENCODING_ID = new MessageSignature(new byte[]{42, 04, 20});
-    private static boolean _active = false;
 
     public static void init() {
         if (Platform.getEnvironment().equals(Env.CLIENT)) {
+            LOGGER.info("Init");
             Config.init(configLocation.toFile());
-            Auth.init();
-            if (Auth.hasCreeperHostAuth() || Platform.isDevelopmentEnvironment()) {
-                _active = true;
-            } else {
-                LOGGER.error("BlockShot will not run in offline mode.");
-            }
+            MineTogetherSession.getDefault().setProvider(new MTSessionProvider());
+            // Trigger session validation early in the background.
+            tokenFuture = MineTogetherSession.getDefault().getTokenAsync();
+            ClientLifecycleEvent.CLIENT_SETUP.register(instance -> clientStart());
         }
-        if (BlockShot.isActive()) {
+    }
+
+    private static void clientStart() {
+        try {
+            JWebToken token = tokenFuture.get();
+            if (token != null) {
+                Auth.init(token);
+                active = true;
+            }
+        } catch (InterruptedException | ExecutionException ignored) {}
+        if (!active) {
+            LOGGER.error("BlockShot will not run in offline mode.");
+        }
+        active |= Platform.isDevelopmentEnvironment();
+        if (active) {
             GuiEvents.init();
             Keybindings.init();
         }
@@ -39,7 +61,7 @@ public class BlockShot {
      * Set on startup if in online mode.
      * */
     public static boolean isActive() {
-        return _active;
+        return active;
     }
 
     public static int getFPS() {
