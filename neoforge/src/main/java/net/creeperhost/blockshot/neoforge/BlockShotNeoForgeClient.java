2b046f17aec757119e27503cde91951b38f8b8c7
Gigabit101 <thegigabit101@gmail.com>
2025-12-10 16:15:05 +0000

    Add support for ftb pause

--- neoforge/src/main/java/net/creeperhost/blockshot/neoforge/BlockShotNeoForgeClient.java
+++ neoforge/src/main/java/net/creeperhost/blockshot/neoforge/BlockShotNeoForgeClient.java
@@ -1,25 +1,18 @@
 package net.creeperhost.blockshot.neoforge;
 
-import net.creeperhost.blockshot.BlockShot;
 import net.creeperhost.blockshot.BlockShotClient;
-import net.creeperhost.blockshot.gui.ModTextures;
-import net.minecraft.resources.ResourceLocation;
+import net.creeperhost.blockshot.integration.Integration;
+import net.creeperhost.blockshot.neoforge.compat.PauseMenuIntegration;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 
 public class BlockShotNeoForgeClient {
     public static void init(IEventBus modBus) {
         modBus.addListener(BlockShotNeoForgeClient::registerKeyMappings);
-        modBus.addListener(BlockShotNeoForgeClient::registerReloadListeners);
+        Integration.runOptional("ftbpmapi", () -> PauseMenuIntegration::init);
     }
 
     private static void registerKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(BlockShotClient.OPEN_GUI);
-    }
-
-    private static void registerReloadListeners(AddClientReloadListenersEvent event)
-    {
-        event.addListener(ResourceLocation.fromNamespaceAndPath(BlockShot.MOD_ID, "textures_reload"), ModTextures.getAtlasHolder());
     }
 }
