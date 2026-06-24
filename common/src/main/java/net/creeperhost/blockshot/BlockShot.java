e4b3040148107dbf5df2e7d434ae7e55b005172a
Paul T <paul_t@creeperhost.net>
2026-06-24 14:24:08 +0100

    Fix production mixin remapping

--- common/src/main/java/net/creeperhost/blockshot/BlockShot.java
+++ common/src/main/java/net/creeperhost/blockshot/BlockShot.java
@@ -8,13 +8,13 @@ import net.covers1624.quack.net.httpapi.HttpEngine;
 import net.covers1624.quack.net.httpapi.java11.Java11HttpEngine;
 import net.creeperhost.blockshot.gui.GuiEvents;
 import net.creeperhost.blockshot.lib.MTSessionProvider;
-import net.creeperhost.blockshot.mixin.MixinMinecraft;
 import net.creeperhost.blockshot.polylib.ModPackInfo;
 import net.creeperhost.minetogether.lib.MineTogetherLib;
 import net.creeperhost.minetogether.lib.web.ApiClient;
 import net.creeperhost.minetogether.lib.web.DynamicWebAuth;
 import net.creeperhost.minetogether.session.JWebToken;
 import net.creeperhost.minetogether.session.MineTogetherSession;
+import net.minecraft.client.Minecraft;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
@@ -81,6 +81,6 @@ public class BlockShot {
     }
 
     public static int getFPS() {
-        return MixinMinecraft.getfps();
+        return Minecraft.fps;
     }
 }
